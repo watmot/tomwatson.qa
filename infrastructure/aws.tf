c20d442cafa6c1593e0033f29a3d314f9b438a72
watmot <tomwatson840@gmail.com>
2023-12-15 10:49:37 +0000

    Create buckets to hold CodePipeline artifacts

--- infrastructure/aws.tf
+++ infrastructure/aws.tf
@@ -18,3 +18,15 @@ resource "aws_s3_bucket_acl" "build_acl" {
   bucket = aws_s3_bucket.build.id
   acl    = "private"
 }
+
+# CodePipeline Artifacts
+resource "aws_s3_bucket" "codepipeline" {
+  for_each = toset(local.build_environments)
+
+  bucket = "${local.project_name}-codepipeline-${each.key}"
+}
+
+resource "aws_s3_bucket_acl" "codepipeline_acl" {
+  bucket = aws_s3_bucket.codepipeline.id
+  acl    = "private"
+}
