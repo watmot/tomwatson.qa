a09060564b29113206a2590fc2d66b5fbf618093
watmot <tomwatson840@gmail.com>
2023-12-15 11:04:45 +0000

    Create missing ownership controls resource

--- infrastructure/aws.tf
+++ infrastructure/aws.tf
@@ -14,9 +14,20 @@ resource "aws_s3_bucket" "build" {
   bucket = "${local.project_name}-build-${each.key}"
 }
 
+resource "aws_s3_bucket_ownership_controls" "build" {
+  for_each = local.build_environments
+
+  bucket = aws_s3_bucket.build[each.key].id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "build_acl" {
   for_each = local.build_environments
-  
+
+  depends_on = [aws_s3_bucket_ownership_controls.build]
+
   bucket = aws_s3_bucket.build[each.key].id
   acl    = "private"
 }
@@ -28,8 +39,19 @@ resource "aws_s3_bucket" "codepipeline" {
   bucket = "${local.project_name}-codepipeline-${each.key}"
 }
 
+resource "aws_s3_bucket_ownership_controls" "codepipeline" {
+  for_each = local.build_environments
+
+  bucket = aws_s3_bucket.codepipeline[each.key].id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "codepipeline_acl" {
   for_each = local.build_environments
+
+  depends_on = [aws_s3_bucket_ownership_controls.codepipeline]
 
   bucket = aws_s3_bucket.codepipeline[each.key].id
   acl    = "private"
