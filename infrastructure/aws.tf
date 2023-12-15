3b508436360ec23c0e148bb3662192d70b391158
watmot <tomwatson840@gmail.com>
2023-12-15 10:46:47 +0000

    Add ACL to build bucket

--- infrastructure/aws.tf
+++ infrastructure/aws.tf
@@ -3,8 +3,18 @@ locals {
   build_environments = ["dev", "test", "staging", "production"]
 }
 
+##########
+### S3 ###
+##########
+
+# Build files
 resource "aws_s3_bucket" "build" {
   for_each = toset(local.build_environments)
 
   bucket = "${local.project_name}-build-${each.key}"
+}
+
+resource "aws_s3_bucket_acl" "build_acl" {
+  bucket = aws_s3_bucket.build.id
+  acl    = "private"
 }
