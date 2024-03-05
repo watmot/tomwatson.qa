abc464fd25b0af659079308dc8590aa1a35da154
watmot <tomwatson840@gmail.com>
2024-03-05 18:23:58 +0000

    Only test View with component tests
    Integration with global state etc can be tested with e2e tests

--- app/src/components/PageHome/PageHome.cy.tsx
+++ app/src/components/PageHome/PageHome.cy.tsx
@@ -1,8 +1,8 @@
-import { PageHome } from '.';
+import { View as PageHome } from './PageHome.view';
 
 describe('PageHome', () => {
   it('should display a header', () => {
-    cy.mount(<PageHome />);
+    cy.mount(<PageHome title="tomwatson.qa website" />);
 
     cy.findByRole('heading', { name: /tomwatson.qa website/i }).should('be.visible');
   });
