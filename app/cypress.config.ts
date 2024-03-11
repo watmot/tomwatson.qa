b1de8fbd5b2f73fe02f8607d63b111757a04b561
watmot <tomwatson840@gmail.com>
2024-03-11 09:50:36 +0000

    Implement local cypress/jest testing framework

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'cypress';
+import { loadEnvConfig } from '@next/env';
+
+const { combinedEnv, loadedEnvFiles } = loadEnvConfig(process.cwd());
 
 export default defineConfig({
   e2e: {
+    env: { ...combinedEnv, ...loadedEnvFiles },
     baseUrl: 'http://localhost:3000'
   },
-
   component: {
     devServer: {
       framework: 'next',
